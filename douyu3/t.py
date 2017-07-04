0ad9db19a1f2677f52d50df60fa8c94c14563204
unknown <>
2017-07-04 15:14:24 +0800

    fd

---
+++ douyu3/t.py
@@ -0,0 +1,13 @@
+
+from douyu.chat.room import ChatRoom
+
+def on_chat_message(msg):
+    print ('[%s]:%s' % (msg.attr('nn'), msg.attr('txt')))
+
+def run():
+    room = ChatRoom('6324')
+    room.on('chatmsg', on_chat_message)
+    room.knock()
+
+if __name__ == '__main__':
+     run()
